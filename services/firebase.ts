af8f0e787fc7265057dbbd65b46fd67ba43f3f93
Lahiru075 <sanjeewalahiru057@gmail.com>
2026-01-27 19:58:30 +0530

    add .env file

--- services/firebase.ts
+++ services/firebase.ts
@@ -5,13 +5,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeAuth , getReactNativePersistence } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtUMcA6fYIw_V9qvSh_X8Iv6yTBw9p4ss",
-  authDomain: "boardingmate.firebaseapp.com",
-  projectId: "boardingmate",
-  storageBucket: "boardingmate.firebasestorage.app",
-  messagingSenderId: "903024643927",
-  appId: "1:903024643927:web:d8babf7976492e19ea3812",
-  measurementId: "G-GD0NJFHLKD"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
